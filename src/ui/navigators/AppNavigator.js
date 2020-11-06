7618a94b702a46793f154bc08ff56ae227afa80b
Graham Francisco <42788240+gbfrancisco@users.noreply.github.com>
2020-11-05 17:41:49 -1000

    fixed bugs, and installed stack navigator

--- src/ui/navigators/AppNavigator.js
+++ src/ui/navigators/AppNavigator.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-//import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
 import EmployeeCheck from '../screens/EmployeeCheck';
 import Home from '../screens/Home';
 
