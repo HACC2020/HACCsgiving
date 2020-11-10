1156b509dde3d698eb26634298340fa0b6600180
Graham Francisco <42788240+gbfrancisco@users.noreply.github.com>
2020-11-09 22:20:16 -1000

    Added icons to tab navigation

--- src/ui/navigators/SplashNavigator.js
+++ src/ui/navigators/SplashNavigator.js
@@ -4,14 +4,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Splash from '../screens/Splash';
 import Search from '../screens/Search';
 import Services from '../screens/Services';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator();
 
 const SplashNavigator = (props) => {
   return (
     <Tab.Navigator
-      tabBarOptions={{
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, size }) => {
+          let iconName;
 
+          if (route.name === 'Home') {
+            iconName = focused ? 'home-sharp' : 'home-outline';
+          } else if (route.name === 'New Search') {
+            iconName = focused ? 'search-sharp' : 'search-outline';
+          } else if (route.name === 'All Services') {
+            iconName = focused ? 'information-circle-sharp' : 'information-circle-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} />;
+        },
+      })}
+      tabBarOptions={{
+        labelStyle: {
+          fontSize: 14,
+        }
       }}
     >
       <Tab.Screen name="Home" component={Splash} />
