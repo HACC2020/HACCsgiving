91410378903c6d0af4121f59168ca3c26beb8f0b
Graham Francisco <42788240+gbfrancisco@users.noreply.github.com>
2020-11-11 01:10:14 -1000

    Fixed search bar

--- src/ui/screens/Search.js
+++ src/ui/screens/Search.js
@@ -1,12 +1,97 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, ActivityIndicator, FlatList, StyleSheet } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
+import firestore from '@react-native-firebase/firestore';
 
 const Search = (props) => {
+
+  //const [query, setQuery] = useState('');
+  const [filteredServices, setFiltered] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [services, setServices] = useState([]);
+  const [search, updateSearch] = useState('');
+  const ref = firestore().collection('services');
+
+  const searchFilter = (text) => {
+    let newData = services.filter((item) => {
+      console.log(item);
+      let itemData = `${item.title.toUpperCase()}`;
+      // ${item.body.toUpperCase()}`;
+
+      let textData = text.toUpperCase();
+
+      return itemData.indexOf(textData) > -1;
+    });
+
+    setFiltered(newData);
+  };
+
+  const renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%',
+        }}
+      />
+    );
+  };
+
+  useEffect(() => {
+    return ref.onSnapshot(querySnapshot => {
+      const list = [];
+      querySnapshot.forEach(doc => {
+        const { title, body, link } = doc.data();
+        list.push({
+          id: doc.id,
+          title,
+          body,
+          link,
+        });
+      });
+
+      setServices(list);
+      // console.log(list);
+      if (loading) {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={{ flex: 1 }}>
+      <SearchBar
+        placeholder="Type Here..."
+        lightTheme
+        round
+        onChangeText={(text) => {
+          updateSearch(text);
+          searchFilter(text)
+        }}
+        value={search}
+        autoCorrect={false}
+      />
+      <FlatList
+        data={filteredServices}
+        renderItem={({ item }) => (
+          <Text>
+            {item.title}
+          </Text>
+        )}
+        keyExtractor={item => item.title}
+      />
     </View>
-  )
+  );
 };
 
 export default Search;
