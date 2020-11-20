6a90f62bf3e3c2af780066ae629852f24e0b1195
Calvan Liang <42818741+calvan-liang@users.noreply.github.com>
2020-11-20 10:42:47 -1000

    ServiceDetails.js UI Update

--- src/ui/screens/ServiceDetails.js
+++ src/ui/screens/ServiceDetails.js
@@ -1,71 +1,119 @@
 import React, { useCallback } from 'react';
-import { Button, Text, View, Alert, Linking, StyleSheet } from 'react-native';
+import { Button, Text, TouchableOpacity, View, Alert, Linking, StyleSheet } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import Card from '../components/Card';
 import { COLORS } from '../../config/Colors';
-
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ServiceDetails = ({ navigation, route }) => {
-    const url = route.params.service.link;
+  const url = route.params.service.link;
 
-    const handleLink = useCallback(async () => {
-        const supported = await Linking.canOpenURL(url);
+  const handleLink = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url);
 
-        if (supported) {
-            // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-            // by some browser in the mobile
-            await Linking.openURL(url);
-            //tell firestore someone clicked a link
-            firestore()
-                .collection('linkclick')
-                .add({
-                    serviceId: route.params.service.id,
-                })
-                .then(() => {
-                    console.log('logged link click!');
-                });
-        } else {
-            Alert.alert(`Don't know how to open this URL: ${url}`);
-        }
-    }, [url]);
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(url);
+      //tell firestore someone clicked a link
+      firestore()
+        .collection('linkclick')
+        .add({
+          serviceId: route.params.service.id,
+        })
+        .then(() => {
+          console.log('logged link click!');
+        });
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
 
-    return (
-        <View style={styles.container}>
-            <Card>
-                <Text style={styles.cardText}>{route.params.service.title}</Text>
-            </Card>
-            <Card>
-                <Text  numberOfLines={25} style={StyleSheet.paragraph}>{route.params.service.body}</Text>
-            </Card>
-            <Button title="link to service" onPress={handleLink} />
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{route.params.service.title}</Text>
+      </View>
+
+      <ScrollView>
+        <View style={styles.paragraphContainer}>
+          <Text numberOfLines={20} style={styles.paragraph}>{route.params.service.body}</Text>
         </View>
-    )
+        <View style={{ flex: 1 }}>
+          <View style={{ ...styles.buttonContainer, marginTop: 16 }}>
+            <TouchableOpacity
+              onPress={handleLink}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Link to service</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ ...styles.buttonContainer, marginVertical: 16 }}>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Go back</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingTop: 30,
-        backgroundColor: COLORS.mainGreen,
-        padding: 8,
-    },
-    cardText: {
-        color: COLORS.black,
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        textAlign: 'center',
-    },
-    bottom: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        marginBottom: 36
-      }
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   paddingTop: 30,
+  //   backgroundColor: '#ecf0f1',
+  //   padding: 8,
+  // },
+  container: {
+    backgroundColor: COLORS.mainGreen,
+    flex: 1
+  },
+  button: {
+    backgroundColor: COLORS.darkGreen,
+    borderRadius: 50,
+    flex: 1,
+    marginHorizontal: 16,
+    padding: 16
+  },
+  buttonContainer: {
+    // backgroundColor: COLORS.black,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  paragraphContainer: {
+    backgroundColor: COLORS.white,
+    borderRadius: 12,
+    marginHorizontal: 16,
+    padding: 16
+  },
+  title: {
+    color: COLORS.white,
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  titleContainer: {
+    justifyContent: 'center',
+    marginVertical: 36
+  },
 });
 
 export default ServiceDetails;
