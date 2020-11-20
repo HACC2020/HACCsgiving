569d27fcb9f4f69bb20ce64d44abefd5c8dd61e8
Graham Francisco <42788240+gbfrancisco@users.noreply.github.com>
2020-11-19 18:59:45 -1000

    Fixed syntax

--- src/ui/screens/Login.js
+++ src/ui/screens/Login.js
@@ -6,55 +6,55 @@ import { login } from '../../firebase/authentication';
 //TODO login errors for incomplete field
 
 export default function Login({ navigation }) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    state = { email: '', password: '', errorMessage: null }
+  let state = { email: '', password: '', errorMessage: null }
 
 
-    return (
-        <View style={styles.container} >
-            <Text>Login</Text>
-            { this.state.errorMessage &&
-                <Text style={{ color: 'red' }}>
-                    {this.state.errorMessage}
-                </Text>
-            }
-            < TextInput
-                style={styles.textInput}
-                autoCapitalize="none"
-                placeholder="Email"
-                onChangeText={(text) => setEmail(text)}
-                value={email}
-            />
-            <TextInput
-                secureTextEntry
-                style={styles.textInput}
-                autoCapitalize="none"
-                placeholder="Password"
-                onChangeText={(text) => setPassword(text)}
-                value={password}
-            />
-            <Button title="Login" onPress={() => login(email, password)} />
-            {/*<Button
+  return (
+    <View style={styles.container} >
+      <Text>Login</Text>
+      {state.errorMessage &&
+        <Text style={{ color: 'red' }}>
+          {state.errorMessage}
+        </Text>
+      }
+      < TextInput
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        secureTextEntry
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <Button title="Login" onPress={() => login(email, password)} />
+      {/*<Button
                 title="Don't have an account? Sign Up"
                 onPress={() => navigation.navigate('SignUp')}
             />*/}
-        </View >
-    );
+    </View >
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    textInput: {
-        height: 40,
-        width: '90%',
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 8
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 8
+  },
 })
